aae43f991d5a59711f2f6b7ceaddb15a98a352b6
Максим Портнов <maximportnov99@yandex.ru>
2024-01-21 20:35:22 +0300

    http status

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -6,7 +6,7 @@ from . import app, db
 
 
 class InvalidAPIUsage(Exception):
-    status_code = HTTPStatus.NOT_FOUND
+    status_code = HTTPStatus.BAD_REQUEST
 
     def __init__(self, message, status_code=None):
         super().__init__()
